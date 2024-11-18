a5a36051dfb229308dd20c146e27925fe67fcf9e
Hidenori Miyoshi <miyohide@gmail.com>
2024-11-18 22:26:23 +0900

    generate spring project

---
+++ src/main/java/com/github/miyohide/sb_with_cognito/SbWithCognitoApplication.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.sb_with_cognito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWithCognitoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbWithCognitoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/miyohide/sb_with_cognito/SbWithCognitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.sb_with_cognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbWithCognitoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
