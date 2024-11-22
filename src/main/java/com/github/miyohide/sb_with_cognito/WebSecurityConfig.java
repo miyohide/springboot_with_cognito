705ac2e1c0290a6e4a688c4d3fb96811db61e5a8
Hidenori Miyoshi <miyohide@gmail.com>
2024-11-22 16:39:59 +0900

    OAuth認証用のコードに変更

--- src/main/java/com/github/miyohide/sb_with_cognito/WebSecurityConfig.java
+++ src/main/java/com/github/miyohide/sb_with_cognito/WebSecurityConfig.java
@@ -2,40 +2,23 @@ package com.github.miyohide.sb_with_cognito;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-            .authorizeRequests((requests) -> requests
-                    .requestMatchers("/", "/home").permitAll()
-                    .anyRequest().authenticated()
-            )
-            .formLogin((form) -> form
-                    .loginPage("/login")
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(Customizer.withDefaults())
+            .authorizeRequests(authz -> authz.requestMatchers("/")
                     .permitAll()
-            )
-            .logout((logout) -> logout.permitAll());
+                    .anyRequest()
+                    .authenticated())
+            .oauth2Login(Customizer.withDefaults())
+            .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutSuccessUrl("/"));
     return http.build();
-  }
-
-  @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails user =
-            User.withDefaultPasswordEncoder()
-                    .username("user")
-                    .password("password")
-                    .roles("USER")
-                    .build();
-    return new InMemoryUserDetailsManager(user);
   }
 }
