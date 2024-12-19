a8dc74aefbc2f80e8a36ac07e565eac0102c59da
Hidenori Miyoshi <miyohide@gmail.com>
2024-12-19 22:41:50 +0900

    データベースの設定としてテナントIDの設定をするためのクラスを作成

---
+++ src/main/java/com/github/miyohide/sb_with_cognito/DatasourceConfig.java
@@ -0,0 +1,27 @@
+package com.github.miyohide.sb_with_cognito;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatasourceConfig {
+  @Bean
+  public TenantAwareDataSource dataSource(
+          TenantRequestContext tenantRequestContext,
+          @Value("${db.user}") String user,
+          @Value("${db.password}") String password,
+          @Value("${db.url}") String url
+  ) {
+    return new TenantAwareDataSource(
+            tenantRequestContext,
+            DataSourceBuilder.create()
+                    .username(user)
+                    .password(password)
+                    .driverClassName("org.postgresql.Driver")
+                    .url(url)
+                    .build()
+    );
+  }
+}
