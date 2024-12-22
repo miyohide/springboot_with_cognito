aaba781116c4fdad5d549656eb0b9179fb1c5342
Hidenori Miyoshi <miyohide@gmail.com>
2024-12-22 21:38:40 +0900

    不要な処理を削除

--- src/main/java/com/github/miyohide/sb_with_cognito/RequestInterceptor.java
+++ src/main/java/com/github/miyohide/sb_with_cognito/RequestInterceptor.java
@@ -4,13 +4,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-
-import java.util.Map;
 
 // リクエストの前処理でcustom:tenant_idを認証情報から取り出し、
 // TenantThreadLocalStorageに格納する
@@ -23,9 +19,7 @@ public class RequestInterceptor implements HandlerInterceptor {
     if (authentication != null && authentication.getPrincipal() instanceof DefaultOidcUser) {
       DefaultOidcUser user = (DefaultOidcUser) authentication.getPrincipal();
       String tenant_id = user.getClaim("custom:tenant_id");
-      System.out.println(tenant_id);
       TenantThreadLocalStorage.setTenantId(tenant_id);
-      System.out.println(TenantThreadLocalStorage.getTenantId());
     }
     return true;
   }
